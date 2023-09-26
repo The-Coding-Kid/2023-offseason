e321c513010d449bd0c4c451aa6ccfba98e4713d
Veerrohit Veeravadivel '26 <veerrohitv08@gmail.com>
2023-09-26 13:09:42 -0700

    fixed some errors in the code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,36 +1,36 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenixpro.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
-  WPI_TalonFX armMotor;
+  private TalonFX armMotor;
 
   //add motors here
   public ArmSubsystem() {
     // TODO: Fix the ID of the arm motor later
-    armMotor = new WPI_TalonFX(0);
+    armMotor = new TalonFX(0);
     armMotor.setInverted(false);
   }
 
   //consider what methods are necessary for the motor to run effectively
     public void setArmPower(double power) {
-        armMotor.set(power);
+        armMotor.set(ControlMode.PercentOutput, power);
     }
     
     public void stopArm() {
-        armMotor.set(0);
+        armMotor.set(ControlMode.PercentOutput, 0);
     }
 
     public void coast() {
         armMotor.setNeutralMode(NeutralMode.Coast);
     }
 
-  public double getPower() {
-    return armMotor.get();
-  }
 
   @Override
   public void periodic() {}
